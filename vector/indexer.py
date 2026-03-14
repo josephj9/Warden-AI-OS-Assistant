122f8ea2f20475d588c69d24b8d8887cc37f6619
josephj9 <sbjoshjose30@gmail.com>
2026-03-14 11:05:11 -0400

    added batches to embedding

--- vector/indexer.py
+++ vector/indexer.py
@@ -21,5 +21,11 @@ def index_folder(folder):
         docs.append(os.path.basename(file))
         metas.append({"file_path": os.path.abspath(file)})
 
-    if ids:
-        collection.add(ids=ids, documents=docs, metadatas=metas)
+    BATCH_SIZE = 5000
+    for i in range(0, len(ids), BATCH_SIZE):
+        batch_ids = ids[i:i+BATCH_SIZE]
+        batch_docs = docs[i:i+BATCH_SIZE]
+        batch_metas = metas[i:i+BATCH_SIZE]
+        
+        print(f"Indexing batch {i//BATCH_SIZE + 1}")
+        collection.add(ids=batch_ids, documents=batch_docs, metadatas=batch_metas)
